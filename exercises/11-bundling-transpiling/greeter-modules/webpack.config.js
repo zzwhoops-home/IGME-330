2ee8552e0358c3029c20cbf65f081a1bf1770af5
zzwhoops-home <zd@5rock.org>
2025-11-09 18:45:09 -0500

    finish bundling/transpiling

--- exercises/11-bundling-transpiling/greeter-modules/webpack.config.js
+++ exercises/11-bundling-transpiling/greeter-modules/webpack.config.js
@@ -1,7 +1,21 @@
 module.exports = {
-    mode: 'development',
+    mode: 'production',
     entry: ['./src/main.js'],
     output: {
         filename: './bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
 };
